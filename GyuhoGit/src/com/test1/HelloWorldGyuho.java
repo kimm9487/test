337efe7bf6d0c80693f5fcbe513d34a4bf0e043e
22-ncore-013g <22-ncore-013g@22-ncore-013>
2023-06-23 17:32:08 +0900

    5454

--- GyuhoGit/src/com/test1/HelloWorldGyuho.java
+++ GyuhoGit/src/com/test1/HelloWorldGyuho.java
@@ -4,7 +4,7 @@ public class HelloWorldGyuho {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕 진짜 개 좆같네 시발");
+		System.out.println("안녕 진짜 개 좆같네 시562562발");
 	}
 
 }
